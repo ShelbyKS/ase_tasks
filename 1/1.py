c9d68e925ebb5e67e9be65bb12e3b5d012a21e91
ShelbyKS <krylov.sanches@mail.ru>
2023-08-27 11:47:47 +0300

    first task done

--- 1/1.py
+++ 1/1.py
@@ -1,1 +1,11 @@
-print("ASD")
+f = open('farm.txt', 'r')
+
+animals = []
+
+for i in f:
+    animals.append(int(i))
+
+sum_legs = animals[0] * 4 + animals[1] * 4 + animals[2] * 2
+print(sum_legs)
+
+f.close()
